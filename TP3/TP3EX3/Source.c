23b1c21c2d3a445e4282c15058a7e37eb1b50228
Foucauld BERGERAULT <foucauld.bergerault@student.yncrea.fr>
2020-09-30 17:52:28 +0200

    ndjnd

--- TP3/TP3EX3/Source.c
+++ TP3/TP3EX3/Source.c
@@ -1,5 +1,71 @@
 #include<stdlib.h>
 #include<stdio.h>
-
+#include<conio.h>
+#define NBMAXNOTE 30
 
 int main() {
+	printf("Entrer vos notes de 0 a 20 : \n");
+	int compteur = 0, nbAbsent =0, nbNoteValide = 0, verif = 0;
+	char reponse;
+	float note, moyenne = 0, tableauNote[30];
+	// Initialisation tableau a moins 2
+	for (int j = 0; j < NBMAXNOTE; j++) {
+		tableauNote[j] = -2;
+	};
+
+	do {
+		// INPUT NOTE
+		printf("\nNote numero %d : ", compteur);
+		scanf_s("%5f",&note);
+		// CAS ou la note n'est pas comprise entre 0 et 20
+		if ((note > 20) || (note < 0)) {
+			printf("Eleve absent ? ou voulez-vous arreter la saisie des notes? (A/O/N)");
+			reponse = _getch();
+			switch (reponse)
+			{
+			case 'A':
+				nbAbsent++;
+				tableauNote[compteur - 1] = -1;
+				break;
+			case 'O':
+				// Verif permet ensuite de compter le nombre de notre entree par la suite
+				verif = compteur;
+				compteur = NBMAXNOTE;
+				
+				break;
+			case'N':
+				compteur--;
+				break;
+			default:
+				compteur = NBMAXNOTE;
+				break;
+				
+			}
+
+		}
+		//Entrer la note dans le tableau
+		else { tableauNote[compteur] = note; };
+		compteur++;
+	} while (compteur < NBMAXNOTE);
+	// Compte le nombre de valide
+	/*if (verif != 0) {
+		nbNoteValide = verif - nbAbsent;
+	} else {
+		nbNoteValide = NBMAXNOTE - nbAbsent;
+	};*/
+	// Clacul de la moyenne
+	for (int i = 0; i < NBMAXNOTE; i++) {
+		if (tableauNote[i] == -2) {
+			break;
+		}
+		if (tableauNote[i] != -1) {
+			moyenne += tableauNote[i];
+		}; 
+		nbNoteValide++;
+
+	};
+	printf("%d", nbNoteValide);
+	nbNoteValide -= nbAbsent;
+	moyenne = moyenne / nbNoteValide;
+	printf("\nIl y a %d notes. La note moyenne est de %f. Il y a %d absent(s)\n",nbNoteValide, moyenne, nbAbsent);
+}
