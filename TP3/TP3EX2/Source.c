b46da9503a7e32939a0faa5db710a7d3da08768c
Foucauld BERGERAULT <foucauld.bergerault@student.yncrea.fr>
2020-09-30 14:55:45 +0200

    exo2

---
+++ TP3/TP3EX2/Source.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main() {
+	unsigned int entier = 2868838400;
+	//a
+	printf("nb octets = %d", sizeof(entier));
+	//b
+	printf("\nnb octets = %d\n", sizeof(entier) * 8);
+	//c
+	
+	for (int i = 0; i < sizeof(entier) * 8; i++) {
+		
+			if (entier & (1 << i)) {
+				printf("Bit  %d : ON\n", i);
+			}
+			else {
+				printf("Bit  %d : OFF\n", i);
+			}
+		
+	}
+
+}
